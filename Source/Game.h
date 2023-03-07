97bec150b424b9cb43c648ab6e5e3a0c6962fd20
Aadi-23 <kumaradarsh24988@gmail.com>
2023-03-07 16:14:54 +0100

    fk it

--- Source/Game.h
+++ Source/Game.h
@@ -1,1 +1,15 @@
-#pragma once
+#include "raylib.h"
+
+struct Entity
+{
+    float speed = 2;
+    Vector2 dir;
+    float size;
+};
+
+class Level
+{
+    Entity Player;
+    void movement();
+    void spawnship();
+};

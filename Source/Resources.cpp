d4206079f40f92947ddd171f811cb26b6cc85084
Aadi-23 <kumaradarsh24988@gmail.com>
2023-03-22 17:14:49 +0100

    added textures

--- Source/Resources.cpp
+++ Source/Resources.cpp
@@ -9,6 +9,14 @@ void ResourceManager::LoadResources()
 	textures.ship = LoadTexture("./Resources/Textures/Ship.png");
 	textures.rock = LoadTexture("./Resources/Textures/Rock.png");
 	textures.startbutton = LoadTexture("./Resources/Textures/Start.png");
+	textures.Bullet = LoadTexture("./Resources/Textures/Bullet.png");
+	textures.MediumRock = LoadTexture("./Resources/Textures/MidRock.png");
+	textures.SmallRock = LoadTexture("./Resources/Textures/SmallRock.png");
+	textures.Retry = LoadTexture("./Resources/Textures/Retry.png");
+	textures.ship0 = LoadTexture("./Resources/Textures/Ship0.png");
+	textures.health = LoadTexture("./Resources/Textures/Health.png");
+	textures.Goback = LoadTexture("./Resources/Textures/Goback.png");
+
 }
 
 void ResourceManager::UnloandResources()
